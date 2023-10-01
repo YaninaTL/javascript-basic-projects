1da384b717b23562238ed8f0ef0aee9e0ae3edbf
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-01 23:17:29 +0200

    learning how to use selectors inside the element

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,5 +1,28 @@
 //using selectors inside the element
 
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+  });
+
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
+
+//I'm withing the question, that's my loop
+//Look for a button in my question (not somewhere else) and safe it in a variable btn
+//Then I define that I want to be executed - toggle
+//And I want to hide other questions text - use remove
+
 // traversing the dom
 
 // const btns = document.querySelectorAll(".question-btn");
