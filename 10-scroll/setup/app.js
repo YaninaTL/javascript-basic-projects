a6e52a1f567a3d8acc4f96c3fa9633d57a969af2
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-05 22:23:00 +0200

    use toggle with a dynamic height

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -12,11 +12,24 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
+/*navToggle.addEventListener("click", function () {
   linksContainer.classList.toggle("show-links");
-});
+});*/
 // KNOWN height, so, it's added to css and this works in js
 // if we add one more link -> doesn't work, so, it's not DYNAMIC
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  //height = 0 by default as we hide it
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+//height = 150 as we have 3 links, so, if we change the quantity it'll be autocalculated
 
 // ********** fixed navbar ************
 
