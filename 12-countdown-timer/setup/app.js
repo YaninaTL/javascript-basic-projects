bdb8bc5352e2e8b8999c4a29f715ea55af2c776d
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-11 21:49:57 +0200

    set up the timer (using ms)

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -52,4 +52,39 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 // shorter way than month
 
-giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am`;
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
+//future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // t - difference btw current and future time
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  //86400000
+  const oneHour = 60 * 60 * 1000;
+  // 3600000
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  // let hours = t / oneHour
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  item.forEach(function (item, index) {
+    item.innerHTML = values[index];
+  });
+}
+getRemainingTime();
